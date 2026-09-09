046ebbfe1710a092073f09981507cde1d2531c14
Pranav <pranavpatl2003@gmail.com>
2026-09-09 18:13:27 +0530

    added

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,6 +1,6 @@
 function Weather({ weather }) {
 
-  const icon = weather.weather[0].icon;
+  const icon = weather.weather[0].icon
 
   const iconUrl =
     `https://openweathermap.org/img/wn/${icon}@4x.png`
